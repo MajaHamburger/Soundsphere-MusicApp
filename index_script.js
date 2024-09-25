7894f30631613eac4f7caa89a06c35f47141d614
MajaHamburger <146946192+MajaHamburger@users.noreply.github.com>
2024-09-25 14:16:25 +0200

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
         1170, 408, 132, 574, 719, 1104, 240, 420, 800, 850
     ];
     //API-Schlüssel und Host-URL für Deezer API
-    const apiKey = "38b065bafemsh4592292504f0750p1ddfc0jsn6d0f55dc976e";
+    const apiKey = "";
     const apiHost = "deezerdevs-deezer.p.rapidapi.com";
 
     //Funktion zur zufälligen Auswahl einer Künstler-ID aus der Liste
